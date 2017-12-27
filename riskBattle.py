e15696add67f62593f174b70e8f9764560453eca
Caleb James Smith <caleb_smith2@baylor.edu>
2017-12-27 13:54:08 -0700

    Add verbosity levels 0, 1, 2, and 3.

--- riskBattle.py
+++ riskBattle.py
@@ -2,6 +2,8 @@ import optparse
 import random
 import time
 
+# Automate Risk Battles
+
 # attackers is total number of attacking armies (must be greater than 1)
 # defenders is total number of defending armies (must be greater than 0)
 # attackerCutoff is a threshold for continuing the attack. The attcker will continue the attack if there are 
@@ -11,7 +13,13 @@ import time
 # Set attackerCutoff = 1 to attack until there is only 1 attacking army remaining or there are no defending armies (all out attack without threshold).
 # attackDice is the number of dice the attacker will roll (must be 1, 2, or 3)
 # defenseDice is the number of dice the defender will roll (must be 1 or 2)
-def battle(attackers, defenders, attackerCutoff, attackDice, defenseDice):
+
+# verbose <= 0: print nothing
+# verbose == 1: print results only
+# verbose == 2: print results and army amounts
+# verbose >= 3: print results, army amounts, and rolls
+
+def battle(attackers, defenders, attackerCutoff, attackDice, defenseDice, verbose=3):
     if not attackers > 1:
         print "The total number of attackers must be greater than 1."
         return
@@ -32,7 +40,8 @@ def battle(attackers, defenders, attackerCutoff, attackDice, defenseDice):
     delay = 0.1
     initialAttackers = attackers
     initialDefenders = defenders
-    print "--- Attackers ---> Defenders --- {0} ---> {1} ---".format(attackers, defenders)
+    if verbose > 1:
+        print "--- Attackers ---> Defenders --- {0} ---> {1} ---".format(attackers, defenders)
     while attackers > attackerCutoff and defenders > 0:
         time.sleep(delay)
         roll += 1
@@ -61,7 +70,8 @@ def battle(attackers, defenders, attackerCutoff, attackDice, defenseDice):
             dice[stringKey] = ""
             for d in dice[key]:
                 dice[stringKey] += "%d " % d
-            print "Roll {0} {1} dice:\t{2}".format(roll, key, dice[stringKey])
+            if verbose > 2:
+                print "Roll {0} {1} dice:\t{2}".format(roll, key, dice[stringKey])
 
         matching = min(numAttackDice, numDefenseDice)
         for i in xrange(matching):
@@ -69,20 +79,25 @@ def battle(attackers, defenders, attackerCutoff, attackDice, defenseDice):
                 defenders -= 1
             else:
                 attackers -= 1
-        print "--- Attackers ---> Defenders --- {0} ---> {1} ---".format(attackers, defenders)
+        if verbose > 1:
+            print "--- Attackers ---> Defenders --- {0} ---> {1} ---".format(attackers, defenders)
     
-    if defenders == 0:
-        print "---------------------------------"
-        print "- The attackers are victorious! -"
-        print "---------------------------------"
-    else:
-        print "---------------------------------"
-        print "- The defenders are victorious! -"
-        print "---------------------------------"
+    if verbose > 0:
+        if defenders == 0:
+            print "---------------------------------"
+            print "- The attackers are victorious! -"
+            print "---------------------------------"
+        else:
+            print "---------------------------------"
+            print "- The defenders are victorious! -"
+            print "---------------------------------"
 
-    print "Number of rolls: {0}".format(roll)
-    print "Initial attackers: {0}, attackers destroyed: {1}, remaining attackers: {2}".format(initialAttackers, initialAttackers - attackers, attackers)
-    print "Initial defenders: {0}, defenders destroyed: {1}, remaining defenders: {2}".format(initialDefenders, initialDefenders - defenders, defenders)
+        print "Number of rolls: {0}".format(roll)
+        print "Initial attackers: {0}, attackers destroyed: {1}, remaining attackers: {2}".format(initialAttackers, initialAttackers - attackers, attackers)
+        print "Initial defenders: {0}, defenders destroyed: {1}, remaining defenders: {2}".format(initialDefenders, initialDefenders - defenders, defenders)
+
+    results = {}
+    return results
 
 if __name__ == "__main__":
     parser = optparse.OptionParser("usage: %prog [options]\n")
@@ -92,12 +107,13 @@ if __name__ == "__main__":
         print "Running simulation of many battles."
     else:
         print "Please input all values as integers."
+        verbose = input("Please input verbosity (0, 1, 2, 3): ")
         attackers = input("Total number of attacking armies (must be greater than 1): ")
         defenders = input("Total number of defending armies (must be greater than 0): ")
         attackerCutoff = input("Continue attack if there are more than this cutoff (must be greater than 0): ")
         attackDice = input("Number of attacking dice (1, 2, or 3): ")
         defenseDice = input("Number of defending dice (1 or 2): ")
-        battle(attackers, defenders, attackerCutoff, attackDice, defenseDice)
+        battle(attackers, defenders, attackerCutoff, attackDice, defenseDice, verbose)
 
 
 
