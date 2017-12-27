92f18ae304d7ba9035a95e97fc8befcbdd386dbd
Caleb James Smith <caleb_smith2@baylor.edu>
2017-12-27 16:08:33 -0700

    Store all results in a list. Perform multiple iterations for each attacker / defender combination.

--- simulation.py
+++ simulation.py
@@ -8,10 +8,18 @@ def simulation():
     attackDice = 3
     defenseDice = 2
     verbose = 0
+    iterations = 100
+    resultList = []
     for attackers in attackerList:
         for defenders in defenderList:
-            results = battle(attackers, defenders, attackerCutoff, attackDice, defenseDice, verbose)
-            print results
+            for iteration in xrange(1, iterations + 1):
+                results = battle(attackers, defenders, attackerCutoff, attackDice, defenseDice, verbose)
+                results["iteration"] = iteration
+                #print results
+                resultList.append(results)
+
+    number_battles = len(resultList)
+    print "Number of battles: {0}".format(number_battles)
 
 if __name__ == "__main__":
     simulation()
